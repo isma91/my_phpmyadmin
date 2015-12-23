bc66dd9ed9c71f43fb0889b8a70ab9a483fcd5ee
isma91 <ismaydogmus@gmail.com>
2015-12-23 10:54:28 +0100

    adding the connection in view

---
+++ views/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="Welcome to My_PHPMyAdmin" />
+  <title>Welcome to My_PHPMyAdmin</title>
+  <link media="all" type="text/css" rel="stylesheet" href="./media/css/mui.min.css" />
+  <link media="all" type="text/css" rel="stylesheet" href="./media/css/materialize.min.css" />
+  <link media="all" type="text/css" rel="stylesheet" href="./media/css/google_material_icons.css" />
+  <link media="all" type="text/css" rel="stylesheet" href="./media/css/style.css" />
+  <script src="./media/js/jquery-2.1.4.min.js"></script>
+  <script src="./media/js/mui.min.js"></script>
+  <script src="./media/js/materialize.min.js"></script>
+</head>
+<body>
+  <div class="container">
+    <div class="mui-panel">
+      <img class="activator" src="./media/css/logo.png">
+      <div class="title"><span class="card-title grey-text text-darken-4">Connection</span></div>
+      <div class="row">
+        <div class="input-field col s6">
+          <i class="material-icons prefix">perm_identity</i>
+          <input name="db_username" id="db_username" type="text">
+          <label for="db_username">Username</label>
+        </div>
+        <div class="input-field col s6">
+          <i class="material-icons prefix">security</i>
+          <input name="db_password" id="db_password" type="password" >
+          <label for="db_password">password</label>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
