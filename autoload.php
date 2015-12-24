6741cb080f88cb892cdeac347a92ebba5696eab5
isma91 <ismaydogmus@gmail.com>
2015-12-24 10:58:02 +0100

    adding some info for autoload and conditions for index

--- autoload.php
+++ autoload.php
@@ -1,6 +1,37 @@
 <?php
-function autoload($class) {
-	$class = str_replace('\\', '/', $class);
+/**
+* Autoload.php
+*
+* A simple autoload
+*
+* PHP Version 5.6.14-0+deb8u1 (cli) (built: Oct  4 2015 16:13:10)
+*
+* @category Model
+* @package  Model
+* @author   isma91 <ismaydogmus@gmail.com>
+* @author   Raph <rbleuzet@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+* @link     https://github.com/isma91/my_phpmyadmin/blob/master/autoload.php
+*/
+/**
+* Autoload
+*
+* This function allows to not add include in every file
+*
+* PHP Version 5.6.14-0+deb8u1 (cli) (built: Oct  4 2015 16:13:10)
+*
+* @category Model
+* @package  Model
+* @param    String $class class you want to use
+* @author   isma91 <ismaydogmus@gmail.com>
+* @author   Raph <rbleuzet@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+* @link     https://github.com/isma91/my_phpmyadmin/blob/master/config_sample.php
+* @return   nothing
+*/
+function autoload($class)
+{
+    $class = str_replace('\\', '/', $class);
     include $class . '.php';
 }
 
--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
+/**
+* Index.php
+*
+* The index core of the project
+*
+* PHP Version 5.6.14-0+deb8u1 (cli) (built: Oct  4 2015 16:13:10)
+*
+* @category Controller
+* @package  Controller
+* @author   isma91 <ismaydogmus@gmail.com>
+* @author   Raph <rbleuzet@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+* @link     https://github.com/isma91/my_phpmyadmin/blob/master/index.php
+*/
 session_start();
 if (!file_exists('config.php')) {
-	header('Location: ./install/');
+    header('Location: ./install/');
+} else {
+    include './views/index.php';
 }
 require 'autoload.php';
-
-include './views/index.php';
