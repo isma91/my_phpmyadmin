637dcbbd812567163d75739726ac5856630b01ef
isma91 <ismaydogmus@gmail.com>
2015-12-24 11:56:10 +0100

    adding the js file for index (now empty)

---
+++ media/js/index.js
@@ -0,0 +1,9 @@
+/*jslint browser: true, node : true*/
+/*jslint devel : true*/
+/*global $, document, this*/
+$(document).ready(function(){
+    /*$("button#connection").click(function() {
+        $.post('', {: ''}, function (data, textStatus) {
+        });
+    });*/
+});
