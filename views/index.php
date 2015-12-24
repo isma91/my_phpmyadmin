e04c78313d05f751038c54fc8b313a62da654619
isma91 <ismaydogmus@gmail.com>
2015-12-24 10:04:23 +0100

    adding logo in other folder

--- views/index.php
+++ views/index.php
@@ -4,6 +4,25 @@
   <meta charset="utf-8">
   <meta name="description" content="Welcome to My_PHPMyAdmin" />
   <title>Welcome to My_PHPMyAdmin</title>
+  <!-- begin favicon -->
+  <link rel="apple-touch-icon" sizes="57x57" href="./media/img/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="./media/img/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="./media/img/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="./media/img/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="./media/img/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="./media/img/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="./media/img/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="./media/img/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="./media/img/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="./media/img/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./media/img/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="./media/img/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./media/img/favicon-16x16.png">
+  <link rel="manifest" href="./media/img/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="./media/img/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+  <!-- end favicon -->
   <link media="all" type="text/css" rel="stylesheet" href="./media/css/mui.min.css" />
   <link media="all" type="text/css" rel="stylesheet" href="./media/css/materialize.min.css" />
   <link media="all" type="text/css" rel="stylesheet" href="./media/css/google_material_icons.css" />
@@ -14,8 +33,8 @@
 </head>
 <body>
   <div class="container">
-    <div class="mui-panel">
-      <img class="activator" src="./media/css/logo.png">
+    <div class="mui-panel title">
+      <img class="activator" src="./media/img/logo.png">
       <div class="title"><span class="card-title grey-text text-darken-4">Connection</span></div>
       <div class="row">
         <div class="input-field col s6">
@@ -25,7 +44,7 @@
         </div>
         <div class="input-field col s6">
           <i class="material-icons prefix">security</i>
-          <input name="db_password" id="db_password" type="password" >
+          <input name="db_password" id="db_password" type="password">
           <label for="db_password">password</label>
         </div>
       </div>
