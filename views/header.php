5cc1febecc65c8d15100be71605af20a260a5038
isma91 <ismaydogmus@gmail.com>
2015-12-26 01:12:58 +0100

    separate the header to gain some space for each views

---
+++ views/header.php
@@ -0,0 +1,26 @@
+  <!-- begin favicon -->
+  <link rel="apple-touch-icon" sizes="57x57" href="./media/img/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="./media/img/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="./media/img/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="./media/img/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="./media/img/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="./media/img/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="./media/img/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="./media/img/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="./media/img/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="./media/img/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./media/img/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="./media/img/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./media/img/favicon-16x16.png">
+  <link rel="manifest" href="./media/img/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="./media/img/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+  <!-- end favicon -->
+  <link media="all" type="text/css" rel="stylesheet" href="./media/css/mui.min.css" />
+  <link media="all" type="text/css" rel="stylesheet" href="./media/css/materialize.min.css" />
+  <link media="all" type="text/css" rel="stylesheet" href="./media/css/google_material_icons.css" />
+  <link media="all" type="text/css" rel="stylesheet" href="./media/css/style.css" />
+  <script src="./media/js/jquery-2.1.4.min.js"></script>
+  <script src="./media/js/mui.min.js"></script>
+  <script src="./media/js/materialize.min.js"></script>
--- views/index.php
+++ views/index.php
@@ -4,32 +4,9 @@
   <meta charset="utf-8">
   <meta name="description" content="Welcome to My_PHPMyAdmin" />
   <title>Welcome to My_PHPMyAdmin</title>
-  <!-- begin favicon -->
-  <link rel="apple-touch-icon" sizes="57x57" href="./media/img/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="./media/img/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="./media/img/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="./media/img/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="./media/img/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="./media/img/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="./media/img/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="./media/img/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="./media/img/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192"  href="./media/img/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="./media/img/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="./media/img/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="./media/img/favicon-16x16.png">
-  <link rel="manifest" href="./media/img/manifest.json">
-  <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="./media/img/ms-icon-144x144.png">
-  <meta name="theme-color" content="#ffffff">
-  <!-- end favicon -->
-  <link media="all" type="text/css" rel="stylesheet" href="./media/css/mui.min.css" />
-  <link media="all" type="text/css" rel="stylesheet" href="./media/css/materialize.min.css" />
-  <link media="all" type="text/css" rel="stylesheet" href="./media/css/google_material_icons.css" />
-  <link media="all" type="text/css" rel="stylesheet" href="./media/css/style.css" />
-  <script src="./media/js/jquery-2.1.4.min.js"></script>
-  <script src="./media/js/mui.min.js"></script>
-  <script src="./media/js/materialize.min.js"></script>
+  <?php
+  include './views/header.php';
+  ?>
   <script src="./media/js/index.js"></script>
 </head>
 <body>
@@ -40,13 +17,13 @@
       <div class="row">
         <div class="input-field col s6">
           <i class="material-icons prefix">perm_identity</i>
-          <input name="db_username" id="db_username" type="text">
-          <label for="db_username">Username</label>
+          <input name="username" id="username" type="text">
+          <label for="username">Username</label>
         </div>
         <div class="input-field col s6">
           <i class="material-icons prefix">security</i>
-          <input name="db_password" id="db_password" type="password">
-          <label for="db_password">password</label>
+          <input name="password" id="password" type="password">
+          <label for="password">password</label>
         </div>
       </div>
       <button class="waves-effect waves-teal btn-flat" id="connection">Connection</button>
