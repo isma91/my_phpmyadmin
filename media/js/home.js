fbd57cccce7869d06c0ad0aa77f0450b72a3f51e
isma91 <ismaydogmus@gmail.com>
2015-12-28 17:21:13 +0100

    better comrehension of 'key' of each column and begin to parse to see table status

--- media/js/home.js
+++ media/js/home.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
                     allTables = JSON.parse(allTables);
                     databases = '';
                     $.each(allTables, function (database, tables) {
-                        databases = databases + '<li><strong>' + database + '<div class="mui-divider"></div><span class="databaseLength">' + tables.length + ' Table(s)</span></strong><ul class="tables">';
+                        databases = databases + '<li><strong id="' + database + '">' + database + '<div class="mui-divider"></div><span class="databaseLength">' + tables.length + ' Table(s)</span></strong><ul class="tables">';
                         if (tables.length === 0) {
                             databases = databases + '<li>Empty</li></ul></li>';
                         } else {
@@ -38,6 +38,16 @@ $(document).ready(function () {
                     $('strong', '#sidedrawer').on('click', function() {
                         $(this).next().slideToggle(200);
                     });
+                    $('strong').click(function () {
+                        $.post('api/?showTableStatus', {databaseName: $(this).attr('id')}, function (tableStatus, textStatus) {
+                            if (textStatus === "success") {
+                                tableStatus = JSON.parse(tableStatus);
+                                console.log(tableStatus);
+                                if (tableStatus === false) {
+                                }
+                            }
+                        });
+                    });
                     $('a', 'li', 'ul.tables').click(function () {
                         databaseName = $(this).attr('class').substr($(this).attr('class').search("table_in_") + 9);
                         tableName = $(this).text();
@@ -47,6 +57,21 @@ $(document).ready(function () {
                                 columns = '';
                                 columns = columns + '<table class="mui-table"><thead><tr><th>#</th><th>Field</th><th>Type</th><th>Null</th><th>Key</th><th>Default</th><th>Extra</th></tr></thead><tbody>';
                                 $.each(tableColumns, function (index, column) {
+                                    if (column.Default === null && column.Null === "NO") {
+                                        column.Default = "No default variable";
+                                    }
+                                    if (column.Extra === "") {
+                                        column.Extra = "No extra";
+                                    }
+                                    if (column.Key === "PRI") {
+                                        column.Key = "PRIMARY KEY";
+                                    } else if (column.Key === "UNI") {
+                                        column.Key = "UNIQUE";
+                                    } else if (column.Key === "MUL") {
+                                        column.Key = "MULTIPLE";
+                                    } else if (column.Key === "") {
+                                        column.Key = "Not indexed";
+                                    }
                                     columns = columns + '<tr><td>'+ index + '</td><td>' + column.Field + '</td><td>' + column.Type + '</td><td>' + column.Null + '</td><td>' + column.Key  + '</td><td>' + column.Default + '</td><td>' + column.Extra + '</td></tr>';
                                 });
                                 columns = columns + '</tbody></table>';
