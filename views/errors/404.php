3b2293c3d861d9ac55112380dcfba69986c2c18e
isma91 <ismaydogmus@gmail.com>
2015-12-28 17:54:55 +0100

    adding personnal 404 error page

---
+++ views/errors/404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="Error 404 of My_PHPMyAdmin" />
+  <title>404 My_PHPMyAdmin</title>
+  <?php
+  include 'error_header.php';
+  ?>
+  <link media="all" type="text/css" rel="stylesheet" href="./../media/css/error_style.css" />
+</head>
+<body>
+  <div class="absolute_center">
+    <div class="center_container">
+      <div class="mui-panel">
+        I am so confused, i d'ont recognize this request... 404 Error not found (and Travolta too...)
+      </div>
+      <img src="./../media/img/confused.gif" alt="travolta is confused">
+    </div>
+  </div>
+</body>
+</html>
