e97382ef09680c5aae29fe8b657060c9664d760c
isma91 <ismaydogmus@gmail.com>
2015-12-28 12:35:53 +0100

    adding the controller of all databases

---
+++ controllers/DatabasesController.php
@@ -0,0 +1,107 @@
+<?php
+/**
+* DatabasessController.php
+*
+* File who contain the Database class
+*
+* PHP Version 5.6.14-0+deb8u1 (cli) (built: Oct  4 2015 16:13:10)
+*
+* @category Controller
+* @package  Controller
+* @author   isma91 <ismaydogmus@gmail.com>
+* @author   Raph <rbleuzet@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+* @link     https://github.com/isma91/my_phpmyadmin/blob/master/controllers/DatabasessController.php
+*/
+namespace controllers;
+use models\Db;
+use models\Database;
+/**
+* DatabasesController
+*
+* Class Databases
+*
+* PHP Version 5.6.14-0+deb8u1 (cli) (built: Oct  4 2015 16:13:10)
+*
+* @category Controller
+* @package  Controller
+* @author   isma91 <ismaydogmus@gmail.com>
+* @author   Raph <rbleuzet@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+* @link     https://github.com/isma91/my_phpmyadmin/blob/master/controllers/DatabasesController.php
+*/
+class DatabasesController extends Database
+{
+    function checkRight ()
+    {
+        if (file_exists('config.php')) {
+            $config = include 'config.php';
+        } else {
+            $config = include '../config.php';
+        }
+        $db = new Db();
+        if (!isset($_SESSION['id']) && !isset($_SESSION['token'])) {
+            return 'false';
+        }
+        $get = $db->getDb()->prepare("SELECT * FROM mysql.user WHERE user = :username AND Host = :host");
+        $get->bindParam(':username', $config['databases']['user']);
+        $get->bindParam(':host', $config['databases']['host']);
+        $get->execute();
+        $user_db_right = $get->fetch(\PDO::FETCH_ASSOC);
+        if ($user_db_right) {
+            return json_encode($user_db_right);
+        } else {
+            return 'false';
+        }
+    }
+    function showDatabases ()
+    {
+        $db = new Db();
+        if (!isset($_SESSION['id']) && !isset($_SESSION['token'])) {
+            return 'false';
+        }
+        $get = $db->getDb()->prepare('SHOW DATABASES');
+        $get->execute();
+        $databases = $get->fetchAll(\PDO::FETCH_ASSOC);
+        $allDatabases = array();
+        if ($databases) {
+            foreach ($databases as $database) {
+                array_push($allDatabases, $database["Database"]);
+            }
+            return json_encode($allDatabases);
+        } else {
+            return 'false';
+        }
+    }
+    function showTables ($databaseName)
+    {
+        if (is_array($databaseName)) {
+            $db = new Db();
+            if (!isset($_SESSION['id']) && !isset($_SESSION['token'])) {
+                return 'false';
+            }
+            $allTables = array();
+            foreach ($databaseName as $database) {
+                $get = $db->getDb()->prepare('SHOW TABLES FROM `' . $database . '`');
+                // replace $databaseName by :databaseName
+                //$get->bindParam(':databaseName', $databaseName);
+                $get->execute();
+                $tables = $get->fetchAll(\PDO::FETCH_ASSOC);
+                if (is_array($tables)) {
+                    if (empty($tables)) {
+                        $allTables[$database] = array();
+                    } else {
+                        foreach ($tables as $table) {
+                            $allTables[$database][] = $table["Tables_in_" . $database];
+                        }
+                    }
+                } else {
+                    return 'false';
+                }
+            }
+            return json_encode($allTables);
+        } else {
+            return 'false';
+        }
+    }
+}
